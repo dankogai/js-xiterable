ad9f834c6dac4727cc2a334b483cf05fc09e404f
Dan Kogai <dankogai+github@gmail.com>
2020-07-20 12:36:05 +0900

    fix babel

--- test/01-import.js
+++ test/01-import.js
@@ -1,9 +1,12 @@
-import {Xiterator, xiterator} from '../xiterator.js';
+import {Xiterator, xiterator, range} from '../xiterator.js';
 describe('import', () => {
   it('Xiterator is a function', () => 
     chai.expect(typeof Xiterator).to.equal('function')
   );
   it('xiterator is a function', () =>
     chai.expect(typeof xiterator).to.equal('function')
+  );
+  it('range is a function', () =>
+    chai.expect(typeof range).to.equal('function')
   );
 });
---
+++ test/02-array.js
@@ -0,0 +1,20 @@
+import {Xiterator, xiterator, range} from '../xiterator.js';
+const $      = chai.expect;
+const should = chai.should;
+describe('[...]', () => {
+    it('[...xiterator(range(4))] === [0,1,2,3]', () =>
+       $([...xiterator(range(4))]).to.deep.equal([0,1,2,3])
+      );
+});
+const gen = (n) =>  xiterator(range(n))
+const ary = [...gen(42)];
+describe('.prototype.map', () => {
+    it('[...iter.map(v=>v*v)] === [...iter].map(v=>v*v)', () =>
+       $([...gen(42).map(v=>v*v)]).to.deep.equal(ary.map(v=>v*v))
+    );
+});
+describe('.prototype.filter', () => {
+    it('[...iter.filter(v=>v%2)] === [...iter].filter(v=>v%2)', () =>
+    $([...gen(42).filter(v=>v%2)]).to.deep.equal(ary.filter(v=>v%2))
+    );
+});
